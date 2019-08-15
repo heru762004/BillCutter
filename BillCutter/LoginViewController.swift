4f41550148a2c468719e00425c132d5753696f24
heru762004 <Heru@Heru-Prasetias-MacBook-Pro.local>
2019-08-15 08:02:22 +0800

    Add event register and login button, Modify UI
    Add event register and login button, Modify UI

--- BillCutter/LoginViewController.swift
+++ BillCutter/LoginViewController.swift
@@ -20,7 +20,11 @@ class LoginViewController: UIViewController {
     }
 
     @IBAction func loginPressed(_ sender: Any) {
+        self.performSegue(withIdentifier: "goToMainMenu", sender: self)
     }
     
+    @IBAction func signUpPressed(_ sender: Any) {
+        self.performSegue(withIdentifier: "goToRegister", sender: self)
+    }
 }
 
